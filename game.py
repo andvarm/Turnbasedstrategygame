f52255a567c37db3806bcfd7123d0af7f84b22f7
andvarm <andreas.varmfors@gmail.com>
2023-11-05 18:53:35 +0100

    second commit

--- game.py
+++ game.py
@@ -15,6 +15,34 @@ red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
 
+"""Define what a garrison is"""
+class Garrison:
+    def __init__(self):
+        self.units = {}  # A dictionary to store unit types and their counts
+
+    def add_unit(self, unit_type, count):
+        if unit_type in self.units:
+            self.units[unit_type] += count
+        else:
+            self.units[unit_type] = count
+
+    def remove_unit(self, unit_type, count):
+        if unit_type in self.units:
+            self.units[unit_type] = max(0, self.units[unit_type] - count)
+            if self.units[unit_type] == 0:
+                del self.units[unit_type]
+
+    def get_total_units(self):
+        total = 0
+        for count in self.units.values():
+            total += count
+        return total
+
+    def get_unit_count(self, unit_type):
+        return self.units.get(unit_type, 0)
+    
+
+
 """Define screen"""
 screen = pygame.display.set_mode((width, height))
 
@@ -41,26 +69,101 @@ def display_economy(msg):
     mesg = font_style.render(msg, True, black)
     screen.blit(mesg, [5, 0])
 
+def easy():
+    money = 1200
+    user_garrison1 = Garrison()
+    user_garrison1.add_unit("Musketmen", 120)
+    user_garrison1.add_unit("Musketmen", 120)
+    user_garrison1.add_unit("Musketmen", 120)
+    user_garrison1.add_unit("Musketmen", 120)
+    user_garrison1.add_unit("Musketmen", 120)
+    user_garrison1.add_unit("Musketmen", 120)
+    user_garrison1.add_unit("Musketmen", 120)
+    user_garrison1.add_unit("Spearmen", 200)
+    user_garrison1.add_unit("Spearmen", 200)
+    user_garrison1.add_unit("Spearmen", 200)
+
+    total_units = user_garrison1.get_total_units()
+    musketmen_count = user_garrison1.get_unit_count("Musketmen")
+    spear_men_count = user_garrison1.get_unit_count("Spearmen")
+    
+    print(f"Spearmen count: {spear_men_count}")
+    print(f"Musketmen count: {musketmen_count}")
+    print(f"Total units in the garrison: {total_units}")
+    
+    return money
+
+def medium():
+    money = 800
+    user_garrison1 = Garrison()
+    user_garrison1.add_unit("Musketmen", 120)
+    user_garrison1.add_unit("Musketmen", 120)
+    user_garrison1.add_unit("Musketmen", 120)
+    user_garrison1.add_unit("Musketmen", 120)
+    
+    total_units = user_garrison1.get_total_units()
+    musketmen_count = user_garrison1.get_unit_count("Musketmen")
+    spear_men_count = user_garrison1.get_unit_count("Spearmen")
+
+    print(f"Spearmen count: {spear_men_count}")
+    print(f"Musketmen count: {musketmen_count}")
+    print(f"Total units in the garrison: {total_units}")
+
+    return money
+
+def hard():
+    money = 400
+    user_garrison1 = Garrison()
+    user_garrison1.add_unit("Musketmen", 120)
+    user_garrison1.add_unit("Musketmen", 120)
+    user_garrison1.add_unit("Musketmen", 120)
+    user_garrison1.add_unit("Musketmen", 120)
+
+    total_units = user_garrison1.get_total_units()
+    musketmen_count = user_garrison1.get_unit_count("Musketmen")
+    spear_men_count = user_garrison1.get_unit_count("Spearmen")
+
+    print(f"Spearmen count: {spear_men_count}")
+    print(f"Musketmen count: {musketmen_count}")
+    print(f"Total units in the garrison: {total_units}")
+
+    return money
+
 def check_difficulty(difficulty):
     """Checks what difficulty game is running"""
     money = 0
 
     if difficulty == 1:
-        money = 1200
+        money = easy()
 
     elif difficulty == 2:
-        money = 800
+        money = medium()
     
     elif difficulty == 3:
-        money = 400
+        money = hard()
 
-    return money
+    print("Starting resources set")
+    return (money)
 
+def check_user_country(value):
+    country = ""
 
-def window(difficulty):
+    if value == 1:
+        country = "Sweden"
+
+    elif value == 2:
+        country = "Denmark"
+    
+    elif value == 3:
+        country = "Rome"
+
+    return country
+
+def window(difficulty, country):
     """Draws a window"""
     global money
     money = check_difficulty(difficulty)
+    user_country = check_user_country(country)
 
     pygame.display.set_caption('Imperium Aureum')
 
--- menu.py
+++ menu.py
@@ -8,12 +8,18 @@ pygame.init()
 
 user_name = None
 game_difficulty = 3
+user_country  = 1
 
 def save_user_name(value):
     """Saves username in variable"""
     global user_name
     user_name = value
     print(user_name)
+
+def save_user_country(index, value):
+    global user_country
+    user_country = value
+    print(value)
 
 def set_difficulty(value, difficulty):
     """Saves difficulty"""
@@ -26,7 +32,7 @@ def set_difficulty(value, difficulty):
 def start_the_game():
     """Starts the game"""
     print("Game starts with difficulty: ", game_difficulty)
-    game.window(game_difficulty)
+    game.window(game_difficulty, user_country)
     pass
 
 def menu():
@@ -37,6 +43,7 @@ def menu():
     theme=pygame_menu.themes.THEME_BLUE)
 
     menu.add.text_input('Name: ', default='Karl XII', onchange=save_user_name)
+    menu.add.selector('Country: ', [('Sweden', 1), ('Denmark', 2), ('Rome', 3)], onchange=save_user_country)
     menu.add.selector('Difficulty: ', [('Hard', 3), ('Medium', 2), ('Easy', 1)], onchange=set_difficulty)
     menu.add.button('Play', start_the_game)
     menu.add.button('Quit', pygame_menu.events.EXIT)
